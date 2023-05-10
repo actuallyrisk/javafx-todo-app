81b7fdd021daff5e6967fc5dacd794de6d803d10
Anton Horn <ah259@hdm-stuttgart.de>
2023-05-10 17:00:11 +0200

    TaskManager: connected to db

--- src/main/java/de/todoapp/core/TaskManager.java
+++ src/main/java/de/todoapp/core/TaskManager.java
@@ -1,5 +1,6 @@
 package de.todoapp.core;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Dictionary;
@@ -14,8 +15,13 @@ public class TaskManager {
         tasks = new ArrayList<>();
     }
 
-    public void load(Task[] tasks) {
-        this.tasks = new ArrayList<Task>(Arrays.asList(tasks));
+    public void loadFromDB() {
+        String[][] data = DBHandler.getAllTasks();
+        tasks.clear();
+        for (String[] task: data) {
+            Task t = new Task(Integer.parseInt(task[0]), task[1], task[2], State.values()[Integer.parseInt(task[3])], Date.valueOf(task[4]), Priority.values()[Integer.parseInt(task[5])], Integer.parseInt(task[6]), task[7]);
+            tasks.add(t);
+        }
     }
 
     final Task getTaskByName(String name) {
@@ -27,17 +33,17 @@ public class TaskManager {
         return null;
     }
 
-    public final ArrayList<Task> getTasksByCategoryFlags(long flags) {
+    public final ArrayList<Task> getTasksByCategory(String category) {
         ArrayList<Task> result = new ArrayList<>();
         for (Task task: tasks) {
-            if ((task.getCategoryFlags() & flags) != 0) {
+            if (task.equals(category)) {
                 result.add(task);
             }
         }
         return result;
     }
 
-    public final ArrayList<Task> getTasksByCategoryState(State state) {
+    public final ArrayList<Task> getTasksByState(State state) {
         ArrayList<Task> result = new ArrayList<>();
         for (Task task: tasks) {
             if (task.getState() == state) {
@@ -47,11 +53,15 @@ public class TaskManager {
         return result;
     }
 
-    public void addTask(Task task) {
-        tasks.add(task);
+    public void addTask(String name, String description, State state, Date dueDate, Priority priority, int points, String category) {
+
+        int id = DBHandler.addTask(name, description, state.ordinal(), dueDate.toString(), priority.ordinal(),points, category );
+        tasks.add(new Task(id, name, description, state, dueDate, priority, points, category));
+
     }
 
     public void deleteTask(Task task) {
+        DBHandler.deleteTask(task.getId());
         tasks.remove(task);
     }
 
