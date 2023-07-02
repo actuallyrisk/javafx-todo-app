200a0ef86778ebcfc634f8b1db1a9b199b347aac
Tobias Metzger <contact@tobias-metzger.de>
2023-07-02 16:25:17 +0200

    Added trophy functionality. Some other minor changes.

--- src/main/java/de/todoapp/core/Main.java
+++ src/main/java/de/todoapp/core/Main.java
@@ -47,9 +47,10 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage stage) {
+        // Loads the configuration
+        appConfig.loadConfig();
 
         //We load the database and the fxml files at parallel to save some time at the startup.
-
         // Loads the database
         DatabaseLoader databaseLoader = new DatabaseLoader();
         Thread databaseThread = new Thread(databaseLoader);
